54a17fbeefa9c2d7240e0a3ee24442466c221911
Aleksei <85HopER85@gmail.com>
2020-09-26 14:45:59 +1000

    new version 1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	printf("Hello, world!");
-	printf("Hello, world!");
-	printf("Hello, world!");
+	printf("Hello, world1!");
+	printf("Hello, world2!");
+	printf("Hello, world3!");
 	return 0;
 }
